5feb8cc0a7f1a93917c4c0847d28d469ffe14441
blueyeshop <blueye.shop.bd@gmail.com>
2023-02-07 12:09:28 +0600

    basic-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Blueye.shop</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
